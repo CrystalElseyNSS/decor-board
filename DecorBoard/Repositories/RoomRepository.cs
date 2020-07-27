96a3f39504d55e848a0eaffafd8f799af3a461d1
Crystal Elsey <hello@crystalelsey.com>
2020-07-27 18:32:11 -0500

    created navbar, header, roomform

---
+++ DecorBoard/Repositories/RoomRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DecorBoard.Data;
+using DecorBoard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DecorBoard.Repositories
+{
+    public class RoomRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Room GetByUserId(int userId)
+        {
+            return _context.Room
+                .FirstOrDefault(r => r.UserProfileId == userId);
+        }
+
+        public void Add(Room room)
+        {
+            _context.Add(room);
+            _context.SaveChanges();
+        }
+    }
+}
